a38656ae4b961bd94ea4e4a2087d0ea1b7dc4f2a
Pablo Felipe <pablofelipe@gmail.com>
2026-07-07 13:47:02 -0300

    Add notification_schema to shared Postgres provisioning
    Mirrors the existing schema+grants pattern already used for the other
    five services, giving notification-service its own logically isolated
    schema in the shared Postgres instance.

--- init-scripts/01-create-schemas.sql
+++ init-scripts/01-create-schemas.sql
@@ -21,15 +21,21 @@ COMMENT ON SCHEMA orders_schema IS 'Schema para o serviço de pedidos';
 CREATE SCHEMA IF NOT EXISTS billing_schema;
 COMMENT ON SCHEMA billing_schema IS 'Schema para o serviço de pagamentos';
 
+-- Schema para Notification Service
+CREATE SCHEMA IF NOT EXISTS notification_schema;
+COMMENT ON SCHEMA notification_schema IS 'Schema para o serviço de notificações';
+
 -- Concede permissões para o usuário admin
 GRANT ALL PRIVILEGES ON SCHEMA auth_schema TO admin;
 GRANT ALL PRIVILEGES ON SCHEMA products_schema TO admin;
 GRANT ALL PRIVILEGES ON SCHEMA inventory_schema TO admin;
 GRANT ALL PRIVILEGES ON SCHEMA orders_schema TO admin;
 GRANT ALL PRIVILEGES ON SCHEMA billing_schema TO admin;
+GRANT ALL PRIVILEGES ON SCHEMA notification_schema TO admin;
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA auth_schema TO admin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA products_schema TO admin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA inventory_schema TO admin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA orders_schema TO admin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA billing_schema TO admin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA notification_schema TO admin;
